93cba9d746498cfc638121d071aee7e1bd501731
William Seymour <git@wseymour.co.uk>
2022-09-28 15:39:13 +0100

    WiP

--- server.py
+++ server.py
@@ -58,12 +58,13 @@ def predict():
 	pred = PrefPredict(max_dist, min_common, min_users_prediction)
 	args = request.args
 	uid = args['uid']
-	print(args)
 	total = data.shape[1]
 	text_out = {}
 
-	# add user preferences from the request parameters
 	user = User()
+
+	# add user preferences from the request parameters
+	# answers to Q0
 	user.add_pref(f"{questions[uid][0]}_1", int(args["q0_1"]))
 	user.add_pref(f"{questions[uid][0]}_2", int(args["q0_2"]))
 	user.add_pref(f"{questions[uid][0]}_3", int(args["q0_3"]))
@@ -74,14 +75,39 @@ def predict():
 	user.add_pref(f"{questions[uid][0]}_8", int(args["q0_8"]))
 	user.add_pref(f"{questions[uid][0]}_9", int(args["q0_9"]))
 	user.add_pref(f"{questions[uid][0]}_10", int(args["q0_10"]))
+	
+	# answers to Q1
+	user.add_pref(f"{questions[uid][1]}_1", int(args["q1_1"]))
+	user.add_pref(f"{questions[uid][1]}_2", int(args["q1_2"]))
+	user.add_pref(f"{questions[uid][1]}_3", int(args["q1_3"]))
+	user.add_pref(f"{questions[uid][1]}_4", int(args["q1_4"]))
+	user.add_pref(f"{questions[uid][1]}_5", int(args["q1_5"]))
+	
+	# answers to Q2
+	user.add_pref(f"{questions[uid][2]}_1", int(args["q2_1"]))
+	user.add_pref(f"{questions[uid][2]}_2", int(args["q2_2"]))
+	user.add_pref(f"{questions[uid][2]}_3", int(args["q2_3"]))
+	user.add_pref(f"{questions[uid][2]}_4", int(args["q2_4"]))
+	user.add_pref(f"{questions[uid][2]}_5", int(args["q2_5"]))
+
+	# answers to Q3
+	user.add_pref(f"{questions[uid][3]}_1", int(args["q3_1"]))
+	user.add_pref(f"{questions[uid][3]}_2", int(args["q3_2"]))
+	user.add_pref(f"{questions[uid][3]}_3", int(args["q3_3"]))
+	user.add_pref(f"{questions[uid][3]}_4", int(args["q3_4"]))
+	user.add_pref(f"{questions[uid][3]}_5", int(args["q3_5"]))
+	user.add_pref(f"{questions[uid][3]}_6", int(args["q3_6"]))
 
 	print(user.known_pref)
 
 	# make predictions
 	for i in range(3):
-		q = randint(0, total - 1)
-		q_name = data.columns[q]
-		q_text = data.iloc[0][q]
+		while True:
+			q = randint(0, total - 1)
+			q_name = data.columns[q]
+			q_text = data.iloc[0][q]
+			if int(q_name.split("_")[0][1:]) <= 147:
+				break
 		print("Predicting", q_name, q_text)
 		p = pred.predict(user, q_name)
 		print(p, round(p))
@@ -99,7 +125,7 @@ def get_question(num_answers):
 		q_name = data.columns[q].split("_")[0]
 		q_text = data.iloc[0][q].split(":")[0]
 
-		if q_name != "Q288":
+		if int(q_name[1:]) <= 147:
 			if data[f"{q_name}_1"][0].find("Your Parents") >= 0 and num_answers == 10:
 				break
 			elif data[f"{q_name}_1"][0].find("Assistant Provider") >= 0 and num_answers == 5:
